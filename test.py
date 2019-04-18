f1d0c4c35f999ed47b76ac3ff768e0e3108db4ac
Karan Erry <karan.erry@gmail.com>
2019-04-18 00:10:14 -0400

    adding basic test setup with travis-ci config file

---
+++ test.py
@@ -0,0 +1,14 @@
+from app import app
+import unittest
+
+class FlaskTestCase(unittest.TestCase):
+
+	# Ensure that flask was set up correctly
+	def test_index(self):
+		tester = app.test_client(self)
+		response = tester.get('/view_all_available')
+		self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+	unittest.main()
